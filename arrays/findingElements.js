0977c9dfc8a168c9428d665e2ff7edf68d09f97e
Ellie-Aghajani <z.aqajani@gmail.com>
2023-06-25 09:07:17 -0700

    includes

--- arrays/findingElements.js
+++ arrays/findingElements.js
@@ -1,3 +1,5 @@
 const numbers = [1, 2, 3, 4];
 console.log('index', numbers.indexOf(1));
 console.log('index', numbers.lastIndexOf(1));
+console.log(numbers.indexOf(1) !== -1);
+console.log(numbers.includes(1));
