daab898afebee8f198962e5f7d3c7b87d11b9657
Ellie-Aghajani <z.aqajani@gmail.com>
2023-12-19 18:45:02 -0800

    const

--- basics/index.js
+++ basics/index.js
@@ -7,3 +7,11 @@ console.log(firstName);
 // be a reserved keyword
 // start with a number
 // have a space or hyphen 
+
+//value of a variable can change
+firstName = 'Danny';
+console.log(firstName);
+
+//use const when you don't need to re-assign the value 
+const username = 'elimo'; 
+console.log(username);
