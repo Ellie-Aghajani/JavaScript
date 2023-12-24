deb50021a14131be1114b1836aefda8a355681f1
Ellie-Aghajani <z.aqajani@gmail.com>
2023-12-24 15:23:36 -0800

    while loop

--- conditionals/index.js
+++ conditionals/index.js
@@ -72,3 +72,10 @@ for (let i = 0; i < 6; i++ ){
 for (let i = 5; i >= 1; i-- ){
     if (i % 2 !== 0) console.log(i);
 }
+//while
+
+let i = 0;
+while( i <= 5) {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+}
