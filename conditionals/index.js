bcdd97cc69113f9a6bb56c4fe90e17946e6cdea0
Ellie-Aghajani <z.aqajani@gmail.com>
2023-12-25 15:11:52 -0800

    infinite loop

--- conditionals/index.js
+++ conditionals/index.js
@@ -86,3 +86,18 @@ do {
     if (1 % 2 !== 0) console.log(i) ;
     i++;
 } while (i <= 5);
+//infinite loop
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+while (true) {
+
+}
+
+let x = 0;
+do {
+ // x++;
+} while (x < 5);
+for (let i = 0; i > 5 ;);
