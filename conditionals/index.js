e08f711430451f24e70b6b374a6a4f30dcc31de6
Ellie-Aghajani <z.aqajani@gmail.com>
2023-12-27 19:48:41 -0800

    show numbers

--- conditionals/index.js
+++ conditionals/index.js
@@ -175,3 +175,13 @@ function checkSpeed(speed){
     return 'OK';
 }
 checkSpeed(20);
+
+//example: even or odd
+
+showNumbers (10);
+function showNumbers (limit) {
+    for (let i = 0; i <= limit; i++) {
+        const message = (i % 2 === 0) ? 'EVEN' : 'ODD'
+        console. log(i, message);
+}
+}
