6f99a0e991cbd0392f8690e60e0f3e84007da007
Ellie-Aghajani <z.aqajani@gmail.com>
2023-12-28 18:45:59 -0800

    average

--- conditionals/index.js
+++ conditionals/index.js
@@ -196,3 +196,46 @@ function countTuthy (array) {
     return counter;
 }
 countTuthy(array);
+// show properties of an object
+
+const movie = {
+    title: 'abc',
+    year:2021,
+    director:'def'
+}
+function showProperties(object){
+    for(let props in movie)
+    console.log(props, movie[props]);
+}
+showProperties(movie);
+
+//calculate average
+function calculateGrade (marks) {
+    let sum = 0;
+    for (let mark of marks)
+    Sum += mark;
+    let average = sum / marks. length;
+    if (average < 60) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    return 'A';
+}
+
+//cleaner code 
+function calculateAverage (array) {
+let sum = 0;
+for (let value of array)
+sum += value;
+return sum / array. length;
+}
+
+function calculateGrade (marks) {
+    const average = calculateverage (marks);
+    if (average < 60) return 'F'; 
+    if (average < 70) return 'D'; 
+    if (average < 80) return 'C'; 
+    if (average < 90) return 'B';
+    return 'A';
+}
+    
