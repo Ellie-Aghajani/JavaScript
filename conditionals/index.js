1fdff3a71af82703adcb0a47e0fcd73bc4257635
Ellie-Aghajani <z.aqajani@gmail.com>
2023-12-28 20:19:49 -0800

    showstars

--- conditionals/index.js
+++ conditionals/index.js
@@ -238,4 +238,16 @@ function calculateGrade (marks) {
     if (average < 90) return 'B';
     return 'A';
 }
-    
+//show stars in rows
+
+showStars (10);
+
+function showStars (rows) {
+    for (let row = 1; row <= rows; row++) {
+    let pattern = '';
+    for (let i = 0; i < row; i++)
+    pattern += '*';
+    console. log (pattern);
+    }
+}
+//
