bcb257a9d8f6e5296dc9c773ec1c316a427bedf7
Ellie-Aghajani <z.aqajani@gmail.com>
2023-12-28 20:40:01 -0800

    prime numbers

--- conditionals/index.js
+++ conditionals/index.js
@@ -250,4 +250,19 @@ function showStars (rows) {
     console. log (pattern);
     }
 }
-//
+//show prime numbers
+
+function showPrime (limit){
+    
+    for (let number = 2; number <= limit; number++){
+        let isPrime = true;
+        for(let factor = 2; factor < number; factor++){
+            if (number % factor === 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) console.log(number);
+    }
+}
+showPrime(10);
