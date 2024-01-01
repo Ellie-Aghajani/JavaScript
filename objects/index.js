854e669c6e3ea6964bb98598df66525245bc496a
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-01 07:13:58 -0800

    factory function

--- objects/index.js
+++ objects/index.js
@@ -13,3 +13,21 @@ const circle = {
 }
 
 circle.draw(); //method
+
+//factory function: produces objects
+function createCircle (radius) {
+    return {
+        //radius = radius
+        radius,
+        //function draw (){}
+        draw() {
+            console. log ('draw');
+        }
+    };
+}
+   
+    const circle1 = createCircle (1) ;
+    console.log(circle1);
+    
+    const circle2 = createCircle(2);
+    console.log(circle2);
