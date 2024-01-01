a8e94fb09fcaf26b262cc766a935d53685b19b5e
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-01 06:59:42 -0800

    declare an object

---
+++ objects/index.js
@@ -0,0 +1,15 @@
+//access properties and functions of an object
+
+const circle = {
+    radius: 1,
+    location: {
+        X: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function () {
+    console. log ('draw');
+    }
+}
+
+circle.draw();
