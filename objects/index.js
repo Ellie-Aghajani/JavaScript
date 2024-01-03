6656e3bae1df5dcd6fd291bc08a5807488dd10b4
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-03 07:35:18 -0800

    price range

--- objects/index.js
+++ objects/index.js
@@ -182,4 +182,12 @@ function DraftPost (title, author, body, ){
 }
 const draft = new DraftPost('a', 'b', 'c');
 
-//price rande
+//price range
+let priceRange = [
+    {label: '$', tooltip: 'inexpensive', minPerPerson: 0, maxPerPerson: 10}, 
+    {label: '$$', tooltip: 'moderate', minPerPerson: 11, maxPerPerson: 20}, 
+    {label: '$$$', tooltip: 'expensive', minPerPerson: 21, maxPerPerson: 50}, 
+]
+let restaurant = [
+    {averagePerPerson : 5}
+]
