b863ef2452b0fef925fd2c6e01a9711c965e71a2
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-08 08:44:22 -0800

    add () to show it is not a code block

--- arrays/index.js
+++ arrays/index.js
@@ -110,5 +110,5 @@ console.log(filtered);
 // const items = filtered.map( n => '<li>'+ n + '</li>');
 // const html = '<ul>' + items.join('') + '</ul>';
 // console.log(html);
-const items = filtered.map( n =>{ value: n});
+const items = filtered.map( n =>({ value: n}));
 console.log(items);
