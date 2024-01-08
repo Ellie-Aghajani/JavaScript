64e3833a81be41fa1851650e1337a8fe66c7a6a7
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-08 07:56:35 -0800

    sorting an array

--- arrays/index.js
+++ arrays/index.js
@@ -89,3 +89,16 @@ numbers.sort();
 console.log(numbers);
 numbers.reverse();
 console.log(numbers) ;
+
+//sorting objects in an array
+const courses = [
+    {id : 1, name: 'hi'},
+    {id:2, name: 'hello'}
+];
+courses.sort(function(a, b){
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1 ;
+    return 0;
+
+});
+console.log(courses);
