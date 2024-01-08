9ed13a7459efb628d7246d641274039eb9ca9d8b
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-08 12:04:35 -0800

    example 3

--- arrays/index.js
+++ arrays/index.js
@@ -149,3 +149,11 @@ function includes (array, searchElement) {
             return true;
     return false;
 }
+//example 3
+function except (array, excluded) {
+  const output = [];
+    for (let element of array)
+      if (!excluded.includes(element))
+        output.push(element);
+  return output;
+};
