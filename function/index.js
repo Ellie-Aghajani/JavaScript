8fbc3c5532ed7cd07fef9d00266df99ba2a39574
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-12 14:28:00 -0800

    sum() using reduce method

--- function/index.js
+++ function/index.js
@@ -1,26 +1,30 @@
-// Function Declaration
-function walk() {
-  console.log('walk');
+// // Function Declaration
+// function walk() {
+//   console.log('walk');
+// }
+
+// // Named Function Expression
+// let run = function walk() {
+//   console.log('run');
+// };
+// run();
+
+// //Anonymous Function Expression
+// const run = function() {
+//   console.log('run');
+// }; 
+// //sum
+// function sum(){
+//   let total = 0;
+//   for (let value of arguments){
+//    total += value;
+//   }
+//   return total;
+
+// }
+// console.log(sum(1, 2, 3));
+//rest operator
+function sum(...args){
+  return args.reduce((a, b) => a + b);
 }
-
-// Named Function Expression
-let run = function walk() {
-  console.log('run');
-};
-run();
-
-//Anonymous Function Expression
-const run = function() {
-  console.log('run');
-}; 
-//sum
-function sum(){
-  let total = 0;
-  for (let value of arguments){
-   total += value;
-  }
-  return total;
-
-}
-console.log(sum(1, 2, 3));
-//
+console.log(sum(1, 2, 3, 4, 5));
