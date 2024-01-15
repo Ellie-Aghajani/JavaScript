892126c490aab7ab8df860f69d1314f80cbb070c
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-15 15:08:51 -0800

    set default values

--- function/index.js
+++ function/index.js
@@ -1,30 +1,41 @@
-// // Function Declaration
-// function walk() {
-//   console.log('walk');
-// }
+// Function Declaration
+function walk() {
+  console.log('walk');
+}
 
-// // Named Function Expression
-// let run = function walk() {
-//   console.log('run');
-// };
-// run();
+// Named Function Expression
+let run = function walk() {
+  console.log('run');
+};
+run();
 
-// //Anonymous Function Expression
-// const run = function() {
-//   console.log('run');
-// }; 
-// //sum
-// function sum(){
-//   let total = 0;
-//   for (let value of arguments){
-//    total += value;
-//   }
-//   return total;
+//Anonymous Function Expression
+const run = function() {
+  console.log('run');
+}; 
+//sum
+function sum(){
+  let total = 0;
+  for (let value of arguments){
+   total += value;
+  }
+  return total;
 
-// }
-// console.log(sum(1, 2, 3));
+}
+console.log(sum(1, 2, 3));
+
+
 //rest operator
 function sum(...args){
   return args.reduce((a, b) => a + b);
 }
 console.log(sum(1, 2, 3, 4, 5));
+
+
+//interest rate 
+
+function interest (principal, rate = 3.5 , years ){
+  return principal * rate / 100 * years;
+
+}
+interest(800000, undefined, 15);
