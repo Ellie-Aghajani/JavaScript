a39debb4155f879e12073b869ce2c1c2571d41ea
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-18 17:39:29 -0800

    example

--- function/index.js
+++ function/index.js
@@ -220,3 +220,12 @@ const video = {
   }
 };
 video.showTags();
+
+// example, create sum function
+function sum(...args){
+  if (args.length === 1 && Array.isArray(args[0])){
+    args = [...args[0]]; //spread the array input
+  }
+  return args.reduce((a, b) => a + b);
+}
+console.log(sum(1, 2, 3, 4));
