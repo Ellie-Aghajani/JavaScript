8a2e9c0da2e42a61ba53791a6bdadd97e78d0db4
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-18 17:26:19 -0800

    using bind method

--- function/index.js
+++ function/index.js
@@ -198,3 +198,25 @@ const video = {
   }
 };
 video.showTags();
+// using bind method
+const video = {
+  title : 'a',
+  tags : ['a', 'b', 'c'],
+  showTags(){
+    this.tags.forEach(function (tag){
+      console.log(this.title, tag);
+    }.bind(this)); 
+
+  }
+};
+video.showTags();
+//arrow function inherits this
+// using bind method
+const video = {
+  title : 'a',
+  tags : ['a', 'b', 'c'],
+  showTags(){
+    this.tags.forEach(tag => console.log(this.title, tag)); 
+  }
+};
+video.showTags();
