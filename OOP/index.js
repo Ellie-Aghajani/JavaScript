400be77bb3dcd2fd19c2a4ab80b1ab7dc66a6370
Ellie-Aghajani <z.aqajani@gmail.com>
2024-01-19 18:37:59 -0800

    benefits of OOP

--- OOP/index.js
+++ OOP/index.js
@@ -22,4 +22,8 @@ const employee = {
 }
 console.log(employee.getWage());
 
-//
+//benefits of OOP
+    //Encapsulation: Reduce complexity + increase reusability
+    //Abstraction: Reduce complexity + isolate impact of changes
+    //Inheritance: Eliminate redundant code
+    //Polymorphism: Refactor ugly switch/case statements
