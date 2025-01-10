50404f478d5dfe5cdc9f8c7a6fc52ed01d331fe0
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-09 19:10:39 -0800

    my answer

--- techInterview/index.js
+++ techInterview/index.js
@@ -1,2 +1,17 @@
 //Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples
 //2 of 3 and 5
+
+function fizzbuzz() {
+  for (let i = 0; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log(i, "fizzbuzz");
+    } else if (i % 5 === 0) {
+      console.log(i, "buzz");
+    } else if (i % 3 === 0) {
+      console.log(i, "fizz");
+    } else {
+      console.log(i);
+    }
+  }
+  return;
+}
