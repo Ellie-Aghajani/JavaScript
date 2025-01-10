6d1488ab17e229dd22e0d7037e1bda430c05fc8d
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-09 21:33:30 -0800

    commented one solution

--- techInterview/index.js
+++ techInterview/index.js
@@ -1,20 +1,20 @@
 //Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples
 //2 of 3 and 5
 
-function fizzbuzz(n) {
-  for (let i = 0; i <= 100; i++) {
-    if (n % 5 === 0 && n % 3 === 0) {
-      console.log(n, "fizzbuzz");
-    } else if (n % 5 === 0) {
-      console.log(n, "buzz");
-    } else if (n % 3 === 0) {
-      console.log(n, "fizz");
-    } else {
-      console.log(n);
-    }
-  }
-  return;
-}
+// function fizzbuzz(n) {
+//   for (let i = 0; i <= 100; i++) {
+//     if (n % 5 === 0 && n % 3 === 0) {
+//       console.log(n, "fizzbuzz");
+//     } else if (n % 5 === 0) {
+//       console.log(n, "buzz");
+//     } else if (n % 3 === 0) {
+//       console.log(n, "fizz");
+//     } else {
+//       console.log(n);
+//     }
+//   }
+//   return;
+// }
 
 // another way to answer
 
@@ -29,4 +29,4 @@ const fizzBuzz = () => {
 };
 
 console.log(fizzBuzz());
-console.log(fizzbuzz());
+// console.log(fizzbuzz());
