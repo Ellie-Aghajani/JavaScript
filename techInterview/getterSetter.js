6a3e31a89a39c3f8c4a727d8c4c061e70f3a8489
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-16 09:53:13 -0800

    check typeof value

--- techInterview/getterSetter.js
+++ techInterview/getterSetter.js
@@ -8,10 +8,13 @@ const person = {
     return `${this.firstName} ${this.lastName}`;
   },
   set fullName(input) {
+    //check typeof input
+    if (typeof value !== String) {
+      throw new Error("Invalid input");
+    }
     const parts = input.split(" ");
     if (parts.length !== 2) {
       throw new Error("Enter first and last name");
-      return;
     }
     this.firstName = parts[0];
     this.lastName = parts[1];
