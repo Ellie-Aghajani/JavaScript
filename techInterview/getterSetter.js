ea9bba8ac7a431dbf6515b9cd31e579b96aed5e4
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-16 10:18:29 -0800

    check typeof 'string'

--- techInterview/getterSetter.js
+++ techInterview/getterSetter.js
@@ -2,3 +2,30 @@
 //apply simple error handling
 //check typeof input
 //check if they entered both first and last name
+
+const person = {
+  firstName: "Ellie",
+  lastName: "Aghajani",
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(input) {
+    if (typeof input !== "string") {
+      throw new Error("Enter valid first and Last name");
+    }
+
+    const parts = input.split(" ");
+    if (parts.length !== 2) {
+      throw new Error("Enter both first and last name");
+    }
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+
+try {
+  person.fullName = "";
+  console.log(person.fullName);
+} catch (e) {
+  console.log(e.message);
+}
