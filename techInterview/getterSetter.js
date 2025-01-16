1849b2992d29487a08d5aa32dd7e40fe3e980a7b
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-16 09:40:25 -0800

    function

--- techInterview/getterSetter.js
+++ techInterview/getterSetter.js
@@ -1,2 +1,10 @@
 //write a function which you can access methods of an object and set its vallues.
 //apply simple error analysis
+
+const person = {
+  firstName: "Ellie",
+  lastName: "Aghajani",
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
