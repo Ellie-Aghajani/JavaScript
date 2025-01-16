b9ffdc97cf21ecdc3f9fe2461ccf1a34d3852cc4
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-16 09:55:26 -0800

    completed question

--- techInterview/getterSetter.js
+++ techInterview/getterSetter.js
@@ -1,28 +1,4 @@
 //write a function which you can access methods of an object and set its vallues.
-//apply simple error analysis
-
-const person = {
-  firstName: "Ellie",
-  lastName: "Aghajani",
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-  set fullName(input) {
-    //check typeof input
-    if (typeof value !== String) {
-      throw new Error("Invalid input");
-    }
-    const parts = input.split(" ");
-    if (parts.length !== 2) {
-      throw new Error("Enter first and last name");
-    }
-    this.firstName = parts[0];
-    this.lastName = parts[1];
-  },
-};
-
-try {
-  person.fullName = "E A";
-} catch (e) {
-  alert(e);
-}
+//apply simple error handling
+//check typeof input
+//check if they entered both first and last name
