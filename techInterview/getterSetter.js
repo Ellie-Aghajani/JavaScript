94470aa01df0239c0429bd24969d20b164d2bcbd
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-16 09:56:53 -0800

    edit question

--- techInterview/getterSetter.js
+++ techInterview/getterSetter.js
@@ -1,4 +1,4 @@
-//write a function which you can access methods of an object and set its vallues.
+//write a person object which you can access methods of an object and set its vallues.
 //apply simple error handling
 //check typeof input
 //check if they entered both first and last name
