4004eba2a62fa2ece24d7f567f9aefa5754e4dee
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-16 18:29:51 -0800

    sum of items in an array

--- techInterview/sumOfArguments.js
+++ techInterview/sumOfArguments.js
@@ -1,7 +1,16 @@
 //return sum of the arguments
 
-const sum = (...arguments) => {
-  return arguments.reduce((a, b) => {
+// const sum = (...arguments) => {
+//   return arguments.reduce((a, b) => {
+//     a + b;
+//   });
+// };
+
+// return the sum of items in an array
+
+function sum(...items) {
+  if (items.length === 1 && Array.isArray.items[0]) items = [...items[0]];
+  return items.reduce((a, b) => {
     a + b;
   });
-};
+}
