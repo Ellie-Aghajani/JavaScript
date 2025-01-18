ca2d411e01f224a56b8135338f155a4c8a5ef345
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-18 15:35:42 -0800

    used for loop instead of map

--- techInterview/practice.js
+++ techInterview/practice.js
@@ -1,5 +1,13 @@
 // Question: Write a function to transform an array of numbers by doubling each value.
-
+//solution 1
 const transform = (array) => {
   return array.map((element) => element * 2);
+};
+//solution 2
+const doubledArray = (array) => {
+  const doubled = [];
+  for (let element of array) {
+    doubled.push(element * 2);
+  }
+  return doubled;
 };
