1f68af026e9b95f0caf495be85f936a0f651b55c
Ellie-Aghajani <z.aqajani@gmail.com>
2025-01-17 19:21:17 -0800

    my answer to swap key values in objects

--- techInterview/swapObjectKeyValues.js
+++ techInterview/swapObjectKeyValues.js
@@ -2,3 +2,13 @@
 // Somehow, the properties and keys of the object got mixed up
 // Swap the Javascript object's key with its values and return the resulting object
 // {a: 1 , b : 2, c: 3} returns: {1:"a", 2:"b", 3:"c"}
+
+//obj -> obj[key]= key;
+//swapped -> swapped[key] = obj[key];
+const swap = (obj) => {
+  const swapped = {};
+  for (let key in obj) {
+    swapped[key] = obj[key];
+  }
+  return swapped;
+};
